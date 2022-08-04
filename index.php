296ae8265c4169a75f2161765c321b76bb46f985
Danilo <danilo.nascimento2@hotmail.com>
2022-08-04 22:36:20 +0200

    first try get datas

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 ?>
 
 
+<?php
 
-<p> teste </p>
+$response = wp_remote_get( 'https://api.lotto24.de/drawinfo/6aus49/jackpot' );
+$body = wp_remote_retrieve_body( $response );
+
+$data = json_decode($body);
+var_dump($data);
+
+echo $data->lottery
+
+?>
 
 <?php get_footer(); ?>
