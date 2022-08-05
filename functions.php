2eba18e3f739c1588e884ea2bcdc9bae2438c0a6
Danilo <danilo.nascimento2@hotmail.com>
2022-08-05 13:37:07 +0200

    Function get products and include bootstrap on project

--- functions.php
+++ functions.php
@@ -17,3 +17,4 @@ function lotto_scripts() {
 
 /* Disable WordPress Admin Bar for all users */
 add_filter( 'show_admin_bar', '__return_false' );
+
--- index.php
+++ index.php
@@ -2,17 +2,27 @@
     get_header();
 ?>
 
+<div class="container">
+    <div class="row">
+        teste
+ 
 
 <?php
+$arr = array("6aus49", "eurojackpot", "spiel77", "freiheitplus");
 
-$response = wp_remote_get( 'https://api.lotto24.de/drawinfo/6aus49/jackpot' );
-$body = wp_remote_retrieve_body( $response );
+foreach ($arr as $apikey) {
+    $response = wp_remote_get( 'https://api.lotto24.de/drawinfo/'. $apikey .'/jackpot' );
+    $body = wp_remote_retrieve_body( $response );
+    $data = json_decode($body);
 
-$data = json_decode($body);
-var_dump($data);
+    echo $data->lottery . '<br>';
+};
 
-echo $data->lottery
 
+// $response = wp_remote_get( 'https://api.lotto24.de/drawinfo/6aus49/jackpot' );
+// $body = wp_remote_retrieve_body( $response );
+// $data = json_decode($body);
+// echo $data->lottery
 ?>
 
 <?php get_footer(); ?>
