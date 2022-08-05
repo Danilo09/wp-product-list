453847e95e289259251521124ad9a1b4f2187285
Danilo <danilo.nascimento2@hotmail.com>
2022-08-05 18:49:03 +0200

    Include CSS on functions

--- functions.php
+++ functions.php
@@ -11,9 +11,11 @@
 */
 
 function lotto_scripts() {
-
+    wp_enqueue_style( 'lotto_style',  get_template_directory_uri() .'/assets/css/style.css', array(), null, 'all' );
 
 }
+
+add_action('wp_enqueue_scripts', 'lotto_scripts');
 
 /* Disable WordPress Admin Bar for all users */
 add_filter( 'show_admin_bar', '__return_false' );
