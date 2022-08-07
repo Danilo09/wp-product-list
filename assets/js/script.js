03c7c2ea07dd570da576d446deeee79ba88b310e
Danilo <danilo.nascimento2@hotmail.com>
2022-08-07 19:32:58 +0200

    function change name of products selected

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+function changeNameCards() {
+    const card6aus49 = document.getElementsByName("6aus49");
+    const Freiheit = document.getElementsByName("freiheitplus")
+
+    card6aus49[0].innerText = "LOTTO 6 aus 49";
+    Freiheit[0].innerText = "Freiheit+";
+}
+
+changeNameCards();
+
+
+
+// Mytxt = document.createTextNode("New text");
+// Myspan.appendChild(Mytxt);
